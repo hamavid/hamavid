fb2d8d4a5402af81867ec414fdd9176f4367f612
hamavid <hamavid@Hannahs-computer.local>
2017-10-16 17:54:42 -0700

    landing page about formatting, lotion edits

--- public/js/index.js
+++ public/js/index.js
@@ -43,14 +43,21 @@ var showopac = 0.8
         $('#default').attr('src','images/'+picname+'.'+picsuffix);
         $('#hl_hannah').attr('src','images/'+picname+'_hannah_hl.'+picsuffix);
         $('#hl_aviva').attr('src','images/'+picname+'_aviva_hl.'+picsuffix);
-	// determine which size of about to show
-		if (windowwidth<=450 || $(window).height()<=450) {
-        	$('.smallabout').css('display','block');
-        	$('.largeabout').css('display','none');
-    	} else {
-    		$('.smallabout').css('display','none');
-        	$('.largeabout').css('display','block');
-    	}
+    // show/hide abouts depending on window dims (didn't figure out a CSS version alas)
+        if ((picwidth<=300 & picname=='chair') || (picheight<=390 & picname=='hug')){
+            if (windowwidth >=650 & picname=='hug') {
+                if (picheight>=260){$('.about').css('max-height','260px').css('max-width','520px').css('margin','auto');}
+                else{$('.about').css('max-height','100%').css('max-width','900px').css('margin','auto');}
+            }        
+            else{$('.about').css('max-height','100%').css('max-width','100%').css('margin','0');}
+            $('.smallabout').css('display','block');
+            $('.largeabout').css('display','none');
+        } else {
+            $('.about').css('max-height','80%').css('max-width','70%').css('margin','auto');
+            $('.smallabout').css('display','none');
+            $('.largeabout').css('display','block');
+        }
+        if (picwidth>=550 & picheight>=460){$('.about').css('max-height','350px').css('max-width','385px')}
     // set number of milliseconds to delay after clicking area (more on mobile)    
         wait=0;
         if (windowwidth < 600) {wait=500;}
@@ -71,14 +78,16 @@ var showopac = 0.8
 // Change characteristics of various elements when showabout div is clicked
 	$("#showabout").on('click', function(){
         $('.about').css('opacity',showopac).css('z-index','2');
-        $('.overlay').css('z-index','2');
+        $('.overlay').css('z-index','1');
         $('#showabout').css('display','none');
+        $('#hannah_area,#aviva_area').css('z-index','0');
 	});
 // Change back characteristics of various elements when hideabout x is clicked
 	$(".hideabout,.overlay").on('click', function(){
         $('.about').css('opacity','0').css('z-index','0');
         $('.overlay').css('z-index','0');
         $('#showabout').css('display','block');
+        $('#hannah_area,#aviva_area').css('z-index','2');
         reset_dims_opac();
 	});
 
