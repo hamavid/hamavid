69279a295fd70db0d019859f3c52fe24e1f36a49
hamavid <hamavid@Hannahs-computer.local>
2017-11-13 00:49:47 -0800

    new favicon, testing mobile swiping landing page

--- src/js/index.js
+++ src/js/index.js
@@ -50,14 +50,18 @@ var showopac = 0.8
                 else{$('.about').css('max-height','100%').css('max-width','900px').css('margin','auto');}
             }        
             else{$('.about').css('max-height','100%').css('max-width','100%').css('margin','0');}
+            $('.hannahlabel, .avivalabel').css('font-size','14px').css('border','1px solid black');
             $('.smallabout').css('display','block');
             $('.largeabout').css('display','none');
         } else {
             $('.about').css('max-height','80%').css('max-width','70%').css('margin','auto');
             $('.smallabout').css('display','none');
             $('.largeabout').css('display','block');
+            $('.hannahlabel, .avivalabel').css('font-size','24px').css('border','2px solid black');
         }
         if (picwidth>=550 & picheight>=460){$('.about').css('max-height','350px').css('max-width','385px')}
+        if (picname=='chair'){$('.hannahlabel, .avivalabel').css('margin-top','90%');}
+        else{$('.hannahlabel, .avivalabel').css('margin-top','70%');}
     // set number of milliseconds to delay after clicking area (more on mobile)    
         wait=0;
         if (windowwidth < 600) {wait=500;}
@@ -74,6 +78,43 @@ var showopac = 0.8
 	$("#hannah_area").on('click', function(){
         setTimeout(function() {window.location = "./hannah";}, wait);
     });
+    $('#a-overlay>span').on('click', function(){window.location = "./aviva";});
+    $('#h-overlay>span').on('click', function(){window.location = "./hannah";});
+   
+
+// Bring out overlays on swipe
+   /* // double clicking, for testing on no-touch screen
+       $('body').on('dblclick', function() {
+            if ($('#h-overlay').css('display')=='none' && $('#a-overlay').css('display')=='none') {
+                $('#h-overlay').show('slide', {direction: 'right'}, 500);
+            }
+        });
+       $('#h-overlay').on('dblclick', function() {
+            $('#h-overlay').hide('slide', {direction: 'right'}, 500);
+        });*/
+    // SWIPE  
+    $('body figure').touchwipe({
+      wipeLeft: function() { 
+        if ($('#h-overlay').css('display')=='none' && $('#a-overlay').css('display')=='none') {
+            $('#h-overlay').show('slide', {direction: 'right'}, 500);
+        }
+      },
+      wipeRight: function() { 
+        if ($('#h-overlay').css('display')=='none' && $('#a-overlay').css('display')=='none') {
+            $('#a-overlay').show('slide', {direction: 'left'}, 500);
+        }
+      },
+      min_move_x: 20,min_move_y: 20,preventDefaultEvents: true
+    });
+    $('#h-overlay').touchwipe({
+      wipeRight: function() {$('#h-overlay').hide('slide', {direction: 'right'}, 500);},
+      min_move_x: 20,min_move_y: 20,preventDefaultEvents: true
+    });
+    $('#a-overlay').touchwipe({
+      wipeLeft: function() {$('#a-overlay').hide('slide', {direction: 'left'}, 500);},
+      min_move_x: 20,min_move_y: 20,preventDefaultEvents: true
+    });
+
 
 // Change characteristics of various elements when showabout div is clicked
 	$("#showabout").on('click', function(){
