1162cf49913ecb0480ce2f363004b7e97010f732
Hannah Hamavid <hamavid@Hannahs-computer.local>
2018-10-25 15:43:10 -0700

    sswl top menu bar for all size screens

--- src/hannah/sswl/js/fxns.js
+++ src/hannah/sswl/js/fxns.js
@@ -1,44 +1,34 @@
 $(document).ready(function() {
-// open small nav menu on navbar click and show overlay, close when overlay is clicked or navbars are clicked again
-	$('.navbar-menu, .smallnavlinks, .overlay').click(function(){togglemenu()});
+
+// trigger nav menu/overlay toggling and MENU/X-out functions when various things are clicked
+	$('.navbar-menu, .smallnavlinks, .overlay').click(function(){togglemenu();togglex()});
+
+// show/hide small nav menu and overlay
 	function togglemenu() {
 		$('.smallnavlinks').slideToggle();$('#overlaybkgrnd').fadeToggle();
 	};
+// change "MENU" to "X" and back
+	function togglex() {
+		var now = $('.navbar-menu').html();
+		if (now == 'MENU') {$('.navbar-menu').html('X');}
+		else {$('.navbar-menu').html('MENU');}
+	}	
 
-
-// make sure menus match active tab on menu click
-	$('.smallnavlinks li, .nav-tabs li a').click(function(){matchingmenus(event)});
-	function matchingmenus(event){
+// make sure menu matches active tab on menu click
+	$('.smallnavlinks li').click(function(){matchingmenu(event)});
+	function matchingmenu(event){
 		// get the name of the active tab
-		var activetab = $(event.target).closest('a').attr('href');
-		// remove active/current class from all li elements in all nav menus
-		$('.smallnavlinks li, .nav-tabs li').removeClass('current').removeClass('active');
-		// add active/current class to appropriate li elements in all nav menus
-		// NOTE: I'm using the 'current' class as a substitute for 'active' for styling the menus, 
+		activetab = event.target.parentNode.getAttribute('href');
+		console.log(activetab);
+		// remove active/current class from all li elements in nav menu
+		$('.smallnavlinks li').removeClass('current').removeClass('active');
+		// add active/current class to appropriate li element in nav menu
+		// NOTE: I'm using the 'current' class as a substitute for 'active' for styling the menu, 
 		// but don't actually add 'active' class to original .nav-tabs list elements  
 		// because that messes up whatever bootstrap does with them
 		$('.smallnavlinks a[href="'+activetab+'"] li').addClass('current').addClass('active');
-		$('.nav-tabs a[href="'+activetab+'"]').closest('li').addClass('current');
 	}
 
-
-	// Change style of navbar on scroll or resize
-	$(window).on('scroll', function() {
-		var windowwidth=window.innerWidth || 
-    	document.documentElement.clientWidth || 
-    	document.body.clientWidth;
-		if (window.scrollY > 40 || windowwidth < 750) {
-			$('header').css('height',57);
-			$('.nav-tabs, .nav-tabs li a').addClass('scroll-tabs');
-	        $('.name img').addClass('scroll-name');
-	        $('table.coverimagetable').css('margin-top',57);
-	    } else {
-	    	$('header').css('height',107);
-			$('.nav-tabs, .nav-tabs li a').removeClass('scroll-tabs');
-	        $('.name img').removeClass('scroll-name');
-	        $('table.coverimagetable').css('margin-top',107);
-	    }
-	});
 
 });
 
