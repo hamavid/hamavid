08c6fbea6591b954dcd25ba756dfab435c7c7f29
Hannah Hamavid <hamavid@Hannahs-computer.local>
2018-11-02 15:25:37 -0700

    word count and simpleportfolio updates

--- docs/hannah/blog/wordcount/scripts.js
+++ docs/hannah/blog/wordcount/scripts.js
@@ -2,196 +2,235 @@
 window.onload = function () {
 
 // set globally accessible variables
-var sortselector = document.getElementById('sortby');
-var lastlength;
-document.getElementById('input').onkeydown = function() {
-	lastlength=document.getElementById('input').value.length;return lastlength;
-}
-
-
-// fxn to UNdim the analyze button
-function undim() {
-	if (document.getElementById('input').value.length > 0) {
-		document.getElementById('analyze').classList.remove('dim');
+	var sortselector = document.getElementById('sortby');
+	var lastlength;
+	document.getElementById('input').onkeydown = function() {
+		lastlength=document.getElementById('input').value.length;return lastlength;
 	}
-}
+
+// hide/show explanation
+	var pictogbuttons = document.getElementsByClassName('picture-toggle-button');
+	for (var i = 0; i < pictogbuttons.length; i++) {pictogbuttons[i].onclick=toggleexplanation;}
+	function toggleexplanation() {
+		if (document.getElementById('explanation').style.display == 'block') {
+			document.getElementById('explanation').style.display = 'none';
+			for (var i = 0; i < pictogbuttons.length; i++) {pictogbuttons[i].innerHTML = 'Show explanation';}
+		} else {
+			document.getElementById('explanation').style.display = 'block';
+			for (var i = 0; i < pictogbuttons.length; i++) {pictogbuttons[i].innerHTML = 'Hide explanation';}
+		}
+	}
+
+
+/* ------------ BUTTON DIMMING ---------------*/
+// fxn to UNdim the analyze button
+	function undim() {
+		if (document.getElementById('input').value.length > 0) {
+			document.getElementById('analyze').classList.remove('dim');
+		}
+	}
 // fxn to DIM analyze button
-function dim() {document.getElementById('analyze').classList.add('dim');}
+	function dim() {document.getElementById('analyze').classList.add('dim');}
+
+
+// UNdim analyze button if case check status changes or text in input box changes (and has content)
+	document.getElementById('casesense').onclick = undim;
+	document.getElementById('input').onkeyup = function() {
+		// Get last typed character so we can check that it's not a  space
+		// only undim if not a space or if the last thing that happened was to delete text
+	    var lastChar = String.fromCharCode(this.value.charCodeAt(this.selectionStart - 1));
+	    if (lastChar != " " || this.value.length < lastlength){undim();}
+		if (this.value.length == 0) {dim();} // dim if there is no input text
+	}
+
+
+/* -----IGNORE SECTION ----------*/
+// Change characteristics of various elements when helpignore span is clicked
+	document.getElementById('helpignoreicon').onclick = showhelpignore;
+	function showhelpignore(){
+        document.getElementById('helpignore').style.zIndex = '3';
+        document.getElementsByClassName('ignoreoverlay')[0].style.zIndex = '2';
+        document.getElementById('helpignore').style.opacity = '1';
+        document.getElementsByClassName('ignoreoverlay')[0].style.opacity = '1';
+	};
+// Change back characteristics of various elements when hideignore x is clicked
+	document.getElementById('hideignore').onclick = hidehelpignore;
+	document.getElementsByClassName('ignoreoverlay')[0].onclick = hidehelpignore;
+	function hidehelpignore() {
+       	document.getElementById('helpignore').style.zIndex = '-2';
+        document.getElementsByClassName('ignoreoverlay')[0].style.zIndex = '-2';
+        document.getElementById('helpignore').style.opacity = '0';
+        document.getElementsByClassName('ignoreoverlay')[0].style.opacity = '0';
+	};
 
 
 // functions to add and remove characters from the spans that show ignored chars
 // remove
-var xes = document.querySelectorAll('span.xout');
-for(var i = 0; i < xes.length; i++) {xes[i].onclick=remove_ignore;}
-function remove_ignore(){
-	undim();
-	this.parentNode.parentNode.removeChild(this.parentNode);
-}
-
-// add
-var iglist = document.querySelectorAll('#ignores')[0];
-document.getElementById('addchar').onclick=add_ignore;
-function add_ignore() {
-	var added = document.getElementById('ignore-input').value;
-	// If anything has been added, UNdim the analyze button
-	if (added.length > 0) {undim();}
-	var div = document.createElement('div');
-	var span1 = document.createElement('span');
-	span1.className = 'xout';span1.innerHTML = 'x';div.append(span1);
-	var span2 = document.createElement('span');
-	span2.className = 'ignorchar';span2.innerHTML = added;
-	div.append(span2);iglist.append(div);
-	document.getElementById('ignore-input').value ='';
 	var xes = document.querySelectorAll('span.xout');
 	for(var i = 0; i < xes.length; i++) {xes[i].onclick=remove_ignore;}
-}
+	function remove_ignore(){
+		undim();
+		this.parentNode.parentNode.removeChild(this.parentNode);
+	}
+
+// add
+	var iglist = document.querySelectorAll('#ignores')[0];
+	document.getElementById('addchar').onclick=add_ignore;
+	function add_ignore() {
+		var added = document.getElementById('ignore-input').value;
+		// If anything has been added, UNdim the analyze button
+		if (added.length > 0) {undim();}
+		var div = document.createElement('div');
+		var span1 = document.createElement('span');
+		span1.className = 'xout';span1.innerHTML = 'x';div.append(span1);
+		var span2 = document.createElement('span');
+		span2.className = 'ignorchar';span2.innerHTML = added;
+		div.append(span2);iglist.append(div);
+		document.getElementById('ignore-input').value ='';
+		var xes = document.querySelectorAll('span.xout');
+		for(var i = 0; i < xes.length; i++) {xes[i].onclick=remove_ignore;}
+	}
 
 
 // function to return list of ignored chars as a variable for analysis
-function ignores(){
-	var list = [];
-	var igs = document.querySelectorAll('span.ignorchar');
-	for(var i = 0; i < igs.length; i++){
-		var thisone = igs[i].innerHTML;
-		//list.push('\\'+thisone);
-		list.push(thisone);
+	function ignores(){
+		var list = [];
+		var igs = document.querySelectorAll('span.ignorchar');
+		for(var i = 0; i < igs.length; i++){
+			var thisone = String.raw`${igs[i].innerHTML}`;
+			//list.push('\\'+thisone);
+			list.push(thisone);
+		}
+		return list;
 	}
-	return list;
-}
-
-// UNdim analyze button if case check status changes or text in input box changes (and has content)
-document.getElementById('casesense').onclick = undim;
-document.getElementById('input').onkeyup = function() {
-	// Get last typed character so we can check that it's not a  space
-	// only undim if not a space or if the last thing that happened was to delete text
-    var lastChar = String.fromCharCode(this.value.charCodeAt(this.selectionStart - 1));
-    if (lastChar != " " || this.value.length < lastlength){undim();}
-	if (this.value.length == 0) {dim();} // dim if there is no input text
-}
 
 
+/* -------------- ANALYSIS ----------------*/
 // function to analyze word count
-document.getElementById('analyze').onclick = analyze;
-function analyze() {
-	// dim the analyze button
-	dim();
+	document.getElementById('analyze').onclick = analyze;
+	function analyze() {
+		// dim the analyze button
+		dim();
 
-	// check the input variable
-	var input = document.getElementById('input').value;
+		// check the input variable
+		var input = document.getElementById('input').value;
 
-	// edit/process input
-	var edited = input;
-	// make everything lowercase if we are NOT being case sensitive
-	if (document.getElementById('casesense').checked == false){edited = edited.toLowerCase();}
-	
-	edited = edited.replace(/ {1,}/g,' '); // get rid of double spaces
-	// loop through characters/words to ignore, taking them out of the input string
-	for (var i=0; i<ignores().length; i++) {
-		var re = new RegExp(ignores()[i],'g');
-		edited = edited.replace(re,'');
-	}
+		// edit/process input
+		var edited = input;
+		// make everything lowercase if we are NOT being case sensitive
+		if (document.getElementById('casesense').checked == false){edited = edited.toLowerCase();}
+		
+		edited = edited.replace(/ {1,}/g,' '); // get rid of double spaces
+		// TO DO, maybe: put this in another while/anyleft loop so it doesn't matter what order the ignorables are in
+		// loop through characters/words to ignore, taking them out of the input string
+		for (var i=0; i<ignores().length; i++) {
+			//var re = new RegExp(ignores()[i],'g'); // no regex because of special characters
+			var re = String.raw`${ignores()[i]}`;
+			var anyleft = 1;
+			while (anyleft) {
+				edited = edited.replace(re,'');
+				edited = edited.replace(/ {1,}/g,' ');
+				if (edited.indexOf(re) == -1) {anyleft = 0;}
+			}
+			
+		}
 
 	// get initial numbers/subsets
-	edited = edited.match(/\b(\w+)\b/g);
-	var unique = Array.from(new Set(edited));
+		edited = edited.match(/\b(\w+)\b/g);
+		var unique = Array.from(new Set(edited));
 
 	// loop through unique words, counting their occurrences in the full string and adding to dictionary of results
-	var counts = {}
-	for (var i=0; i<unique.length; i++) {
-		var word = unique[i];
-		var occ = 0;
-		for (var j=0; j<edited.length; j++) {
-			if (edited[j] == word){occ += 1;}
+		var counts = {}
+		for (var i=0; i<unique.length; i++) {
+			var word = unique[i];
+			var occ = 0;
+			for (var j=0; j<edited.length; j++) {
+				if (edited[j] == word){occ += 1;}
+			}
+			counts[word] = occ;	
 		}
-		counts[word] = occ;	
-	}
-	keys = Object.keys(counts);
-	entries = Object.values(counts);
+		keys = Object.keys(counts);
+		entries = Object.values(counts);
 
 	// create table of word-count results
 		// remove old table
-		var oldtable = document.querySelectorAll('.wordtable-outer tbody')[0];
-		var outer = oldtable.parentNode;
-		outer.removeChild(oldtable);
+			var oldtable = document.querySelectorAll('.wordtable-outer tbody')[0];
+			var outer = oldtable.parentNode;
+			outer.removeChild(oldtable);
 		// start a new one and build to it
-		var resultstable = document.createElement('tbody');
-		resultstable.setAttribute('id','wordtable-inner');
-		for (var i=0; i<keys.length; i++) {
-			var newrow = document.createElement('tr');
-			var wordcell = document.createElement('td');
-			newrow.append(wordcell);
-			wordcell.innerHTML = keys[i];
-			var countcell = document.createElement('td');
-			newrow.append(countcell);
-			countcell.innerHTML = entries[i];
-			resultstable.append(newrow)
-		}
+			var resultstable = document.createElement('tbody');
+			resultstable.setAttribute('id','wordtable-inner');
+			for (var i=0; i<keys.length; i++) {
+				var newrow = document.createElement('tr');
+				var wordcell = document.createElement('td');
+				newrow.append(wordcell);
+				wordcell.innerHTML = keys[i];
+				var countcell = document.createElement('td');
+				newrow.append(countcell);
+				countcell.innerHTML = entries[i];
+				resultstable.append(newrow)
+			}
 		// add new table where it goes
-		outer.append(resultstable);
-	
+			outer.append(resultstable);
+		
 	// populate elements and show results div
-	document.getElementById('total').innerHTML = edited.length;
-	document.getElementById('unique').innerHTML = unique.length;
-	document.getElementById('resultsdiv').style.display = 'block';
+		document.getElementById('total').innerHTML = edited.length;
+		document.getElementById('unique').innerHTML = unique.length;
+		//document.getElementById('resultsdiv').style.display = 'block';
 
 	// sort table according to checked preference, if preference is not default (in order of appearance)
-	var value = sortselector.options[sortselector.selectedIndex].value;
-	if (value != 'appear') {trigger_sort(value);}
-};
+		var value = sortselector.options[sortselector.selectedIndex].value;
+		if (value != 'appear') {trigger_sort(value);}
+	};
 
-// SORTING
+/* ------------------ SORTING -----------------*/
 // determine what options are selected from the dropdown, sort accordingly
-sortselector.onchange = function() {
-	var value = sortselector.options[sortselector.selectedIndex].value;
-	trigger_sort(value);
-}
-function trigger_sort(value) {
-	if (value == 'alpha' || value == 'backalpha') {sortTable(0, value);}
-	else if (value == 'freq-asc' || value == 'freq-desc') {sortTable(1, value);}
-	else if (value == 'appear') {analyze();}
-}
+	sortselector.onchange = function() {
+		var value = sortselector.options[sortselector.selectedIndex].value;
+		trigger_sort(value);
+	}
+	function trigger_sort(value) {
+		if (value == 'alpha' || value == 'backalpha') {sortTable(0, value);}
+		else if (value == 'freq-asc' || value == 'freq-desc') {sortTable(1, value);}
+		else if (value == 'appear') {analyze();}
+	}
 
 // fxn to sort table (from https://www.w3schools.com/howto/howto_js_sort_table.asp)
-function sortTable(n, sorttype) {
-	var table, rows, switching, i, x, y, shouldSwitch, sorttype, switchcount = 0;
-	var table = document.querySelectorAll('.wordtable-outer table')[0];
-	var switching = true;
+	function sortTable(n, sorttype) {
+		var table, rows, switching, i, x, y, shouldSwitch, sorttype, switchcount = 0;
+		var table = document.querySelectorAll('.wordtable-outer table')[0];
+		var switching = true;
 	// Make a loop that will continue until no switching has been done
-	while (switching) {
+		while (switching) {
 		// Start by saying: no switching is done
-		switching = false;
-		rows = table.rows;
+			switching = false;
+			rows = table.rows;
 		//Loop through all table rows (except the first, which contains table headers)
-		for (i = 1; i < (rows.length - 1); i++) {
+			for (i = 1; i < (rows.length - 1); i++) {
 			// Start by saying there should be no switching
-			shouldSwitch = false;
+				shouldSwitch = false;
 			// Get the two elements you want to compare, one from current row and one from the next
-			x = rows[i].getElementsByTagName("TD")[n];
-			y = rows[i + 1].getElementsByTagName("TD")[n];
+				x = rows[i].getElementsByTagName("TD")[n];
+				y = rows[i + 1].getElementsByTagName("TD")[n];
 			// Check if the two rows should switch place, based on the direction, flag if so and break loop
-			if (sorttype == "alpha") {
-				if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {shouldSwitch = true;break;}
-			} else if (sorttype == "backalpha") {
-				if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {shouldSwitch = true;break;}
-			} else if (sorttype == "freq-asc") {
-				if (Number(x.innerHTML) > Number(y.innerHTML)) {shouldSwitch = true;break;}
-			} else if (sorttype == "freq-desc") {
-				if (Number(x.innerHTML) < Number(y.innerHTML)) {shouldSwitch = true;break;}
+				if (sorttype == "alpha") {
+					if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {shouldSwitch = true;break;}
+				} else if (sorttype == "backalpha") {
+					if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {shouldSwitch = true;break;}
+				} else if (sorttype == "freq-asc") {
+					if (Number(x.innerHTML) > Number(y.innerHTML)) {shouldSwitch = true;break;}
+				} else if (sorttype == "freq-desc") {
+					if (Number(x.innerHTML) < Number(y.innerHTML)) {shouldSwitch = true;break;}
+				}
+			}
+		// If a switch has been flagged, make the switch and mark that a switch has been done
+			if (shouldSwitch) {
+				rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+				switching = true;switchcount ++; 
 			}
 		}
-		// If a switch has been flagged, make the switch and mark that a switch has been done
-		if (shouldSwitch) {
-			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-			switching = true;switchcount ++; 
-		}
 	}
-}
-
-
 
 
 };
-
-//function case_sense(){var checked = document.getElementById('casesense').checked;}
-//element.className = element.className.replace(/\bmystyle\b/g, "");
 
--- src/hannah/blog/wordcount.md
+++ src/hannah/blog/wordcount.md
@@ -3,31 +3,39 @@ outadir: "../"
 layout: HHblogpost.html
 link: "./wordcount"
 title: "Word count"
-subtitle: ""
-date: 2018-10-11
+subtitle: "How many of each word is there in this text?"
+date: 2018-11-02
 pagetype: BLOG
-draft: true
+draft: false
 ---
 <div>
+<div id='helpignore'>
+<span id="hideignore"><i class="fa fa-lg fa-remove"></i></span><br>
+<div id='helpignorecontent'>A string of characters added to the ignore-list is ignored anywhere it is found, even within a word. For example, ignoring "by" would get rid of the word "by", but also turn "bye" into "e", and "abyss" into "ass". To ignore an entire word, surround it with spaces, for example " by ". If the word is sometimes followed by punctuation, add the word with its punctuation, for example " by, ". If it is the first or last word of your entire input text, add a space before and/or after the input text before clicking Analyze.
+</div>
+</div>
+<!-- overlay so background picture functionality is covered when help section is showing -->
+<div class="ignoreoverlay"></div>	
+
 <div id='inputdiv'>
 	<h3>Input</h3>
 	<p><em><strong>Words to analyze</strong></em><br>
 	<textarea id='input'>But all that seems distant and all  that seems far, From those wonderful nights at the palace of the Czar, hey hey hey I was shootin' with Rasputin, ate farina with Czarina, Blintzes with the princes of the Czar. Hey hey hey We were sharing tea and herring, dipped banana in Smetana, Borscht  and vorscht around the samovar, hey hey</textarea>
 	</p>
 	<p><input type='checkbox' id='casesense'> <label for='casesense'>Case-sensitive</label></p>
-	<p><em><strong>Characters or words to ignore</strong></em><br>
+	<p><em><strong>Character strings to ignore</strong></em> <span id='helpignoreicon'><i class="fa fa-question-circle" aria-hidden="true"></i></span><br>
 	<input id='ignore-input'></input><span id='addchar'>Add</span><br>
 	<div id='ignores'>
 		<div><span class='xout'>x</span><span class='ignorchar'>hey</span></div>
 	</div></p><br>
-	<p style='text-align:center;'><a href='./#resultsdiv' class='vizbutton' id='analyze'>Analyze</a></p>
+	<p style='text-align:center;'><span class='vizbutton' id='analyze'>Analyze</span></p>
 </div>
 <div id='resultsdiv'>
 	<h3>Results</h3>
 	<div class='summarytable'>
 		<table><tbody>
-		<tr><td>Total word count:</td><td id='total'></td></tr>
-		<tr><td>Unique word count:</td><td id='unique'></td></tr>
+		<tr><td>Total word count:</td><td id='total'>--</td></tr>
+		<tr><td>Unique word count:</td><td id='unique'>--</td></tr>
 		</tbody></table>
 	</div><br>
 	<div id='sortby-outer'><span><strong>Sort:</strong> </span>
@@ -42,14 +50,19 @@ draft: true
 	<div class='wordtable-outer'>
 		<table>
 		<thead><th>Word</th><th>Occurrences</th></thead>
-		<tbody></tbody>
+		<tbody><tr><td>--</td><td>--</td></tr></tbody>
 		</table>
 	</div>
-</div>	
 </div>
+<div id='explanation' style='display:block;'>
+<h4>Why did I make this thing?</h4>
+<p>It seems like there is demand for a tool like this, since similar web-apps exist: There's a <a href='http://www.writewords.org.uk/word_count.asp'>Word Frequency Counter</a> that produces an equivalent word-by-word table of results, but is otherwise pretty bare bones. <a href='https://wordcounter.net/'>Wordcounter.net</a> is also a cool site that reports all sorts of things I didn't bother with, like number of characters, keyword density, and estimated reading or speaking time. But it doesn't put out the itemized table. And <a href='https://wordcounttools.com/'>Wordcounttools.com</a> does an analysis of syllables and readability scores, which <a href='https://www.webpagefx.com/tools/read-able/readability-score.html'>turn</a> <a href='https://datayze.com/readability-analyzer.php'>out to be</a> <a href='http://www.readabilityformulas.com/free-readability-formula-tests.php'>a whole</a> <a href='https://readable.io/'>world</a>. There are also many web-apps that do a basic total word and character count. None of the ones I found include the ability to customize the analysis (case sensitivity, ignorable strings) or sort results (by order of appearance, frequency, or The Alphabet). So arguably this tool has some unique features on top of the baseline, apparently desired function, and therefore it is useful if you want to do those things.</p>
 
-<!--
-http://www.writewords.org.uk/word_count.asp
-https://wordcounter.net/
-https://wordcounttools.com/
--->
+<p>On the other hand, this has a lot in common with a word cloud, and <a href='https://www.jasondavies.com/wordcloud/'>there</a> <a href='https://www.wordclouds.com/'>are</a> 
+<a href='https://tagcrowd.com/'>multiple</a> <a href='http://www.wordle.net/'>apps</a> for that. <a href='https://www.maxqda.com/help-max18/visual-tools/word-clouds'>This one</a> in particular looks like it would make a table similar to the one I make here, along with lots of other lovely aesthetic stuff.</p>
+
+<p>And actually I made this because I wanted to know how many different words were in the chorus of a certain song &mdash; the default input text on this post &mdash; and didn't immediately find an existing tool when I searched for one. Specifically, I thought 40 unique words would sound less intimidating than 60 overall words when trying to teach the song. Even better, once you've learned "hey" you're 13% of the way there! Also, a friend had been talking to me about compression algorithms, so ways to make information compact were on the mind. I realized a couple minutes after thinking all this that it was silly on a number of levels, and in no way simplifying things for humans. But by then the wondering was too great and I had to make the app anyway.</p>
+
+<p>If you find a bug or think of a cool thing to add, and you are <a href='https://www.theguardian.com/science/brain-flapping/2017/may/30/why-do-pedants-pedant'>nice and collaborative about it</a>, feel free to email me!</p>
+</div>
+</div>
